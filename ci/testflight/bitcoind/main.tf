fcefb9cb60216e08d2b7f0ba1a5f471e61637aea
bodymindarts <justin@galoy.io>
2025-11-06 16:19:17 +0100

    ci: fix testflight

--- ci/testflight/bitcoind/main.tf
+++ ci/testflight/bitcoind/main.tf
@@ -82,7 +82,7 @@ provider "kubernetes-alpha" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = "https://${data.google_container_cluster.primary.private_cluster_config.0.private_endpoint}"
     token                  = data.google_client_config.default.access_token
     cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
